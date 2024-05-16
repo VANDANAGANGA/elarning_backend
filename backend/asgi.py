32f4b66714c0514c4179d2695e9a8c3a612fced8
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-16 14:41:19 +0530

    Update asgi.py

--- backend/asgi.py
+++ backend/asgi.py
@@ -19,14 +19,10 @@
 #     }
 # )
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
-from accounts.routing import websocket_urlpatterns
 
 def start_application():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+    from django.core.asgi import get_asgi_application
     return get_asgi_application()
 
 application = start_application()
