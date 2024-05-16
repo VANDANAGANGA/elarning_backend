d6f8d09b76f99a5fbb0deaba5021d0003321ea19
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-16 13:40:46 +0530

    Update asgi.py

--- backend/asgi.py
+++ backend/asgi.py
@@ -29,14 +29,4 @@ def start_application():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     return get_asgi_application()
 
-application = ProtocolTypeRouter(
-    {
-        'http': start_application(),
-        'websocket': AuthMiddlewareStack(
-            URLRouter(
-                websocket_urlpatterns
-            )
-        ),
-    }
-)
-
+application = start_application()
