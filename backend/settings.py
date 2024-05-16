96c22f5b97a888a1f38265bb898a24a4e4f14aa3
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-16 11:12:56 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,7 @@ from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 
 # Quick-start development settings - unsuitable for production
@@ -213,7 +214,6 @@ CHANNEL_LAYERS = {
 }
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 app = Celery('backend')
 
