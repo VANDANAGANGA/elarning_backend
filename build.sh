9f4bdcdd3eaf5fd7935acff4d5efa6a0f2e89425
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-23 14:05:31 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ pip install -r requirements.txt
 python manage.py migrate
 
 
+# Debug print statement to check the environment variable
+echo "CREATE_SUPERUSER is set to: $CREATE_SUPERUSER"
+
 # Check if CREATE_SUPERUSER variable is set and not empty
 if [[ -n $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input
