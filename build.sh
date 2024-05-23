8e137840fac8b37a4ac8b97ece9741b21767f7af
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-23 20:11:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+
+# Exit script immediately if a command exits with a non-zero status
 set -o errexit
-pip install -r requirements.txt 
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Apply database migrations
 python manage.py migrate
 
 # Set environment variables for superuser creation
@@ -17,10 +24,13 @@ echo "DJANGO_SUPERUSER_FULL_NAME is set to: $DJANGO_SUPERUSER_FULL_NAME"
 echo "DJANGO_SUPERUSER_PHONE_NUMBER is set to: $DJANGO_SUPERUSER_PHONE_NUMBER"
 
 # Check if CREATE_SUPERUSER variable is set and not empty
-if [[ -n $CREATE_SUPERUSER ]]; then
-  python manage.py createsuperuser_with_details \
-    --email $DJANGO_SUPERUSER_EMAIL \
-    --password $DJANGO_SUPERUSER_PASSWORD \
-    --full_name $DJANGO_SUPERUSER_FULL_NAME \
-    --phone_number $DJANGO_SUPERUSER_PHONE_NUMBER
+if [ -n "$CREATE_SUPERUSER" ]; then
+  python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email="$DJANGO_SUPERUSER_EMAIL").exists():
+    User.objects.create_superuser("$DJANGO_SUPERUSER_EMAIL", "$DJANGO_SUPERUSER_PASSWORD", full_name="$DJANGO_SUPERUSER_FULL_NAME", phone_number="$DJANGO_SUPERUSER_PHONE_NUMBER")
+else:
+    print("Superuser already exists.")
+EOF
 fi
