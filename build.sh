3b11a160c87b904569534449732ca27f8f4c1563
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-23 14:04:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@ set -o errexit
 pip install -r requirements.txt 
 python manage.py migrate
 
-if [[-z $CREATE_SUPERUSER]]; then python manage.py createsuperuser fi
+
+# Check if CREATE_SUPERUSER variable is set and not empty
+if [[ -n $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --no-input
+fi
