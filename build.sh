2839bb339a2b0e236091bcdacb61cc7178f3ec9c
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-23 11:56:35 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ set -o errexit
 pip install -r requirements.txt 
 python manage.py migrate
 
-if [[-z $CREATE_SUPERUSER]]; then python manage.py createsuperuserfi
+if [[-z $CREATE_SUPERUSER]]; then python manage.py createsuperuser
