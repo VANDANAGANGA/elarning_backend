e2e87911bf5f02c39219505cfc4d38818f975e19
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-05-24 09:34:13 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -150,7 +150,13 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Optional: Additional static files directories
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Path to your static files within the project
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
