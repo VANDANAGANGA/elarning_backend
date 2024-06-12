4afac276470f2d1f8799696ee0a90e0555d77dec
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-06-12 21:12:42 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -226,10 +226,11 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL')],
         },
     },
 }
+
 
 
 app = Celery('backend')
