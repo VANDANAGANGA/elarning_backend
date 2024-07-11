fbfb651861f3a16386a01081ddfd0be6c89d39db
VANDANAGANGA <124765242+VANDANAGANGA@users.noreply.github.com>
2024-07-11 12:47:58 +0530

    Update urls.py for adding jwt refresh token

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import  TokenRefreshView
 
 urlpatterns = [
     path('register/', views.UserRegister.as_view(), name='user-registration'),
     path('generate-otp/', views.GenerateOTP.as_view(), name='generate_otp'),
     path('verify-otp/', views.VerifyOTP.as_view(), name='verify_otp'),
-
     path('login/',views.MyTokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/',views.MyTokenLogoutView.as_view()),
+    
     path('courses/',views.CourseView.as_view(),name='courses'),
     path('coursedetailsmain/',views.CourseDetailsAPIView.as_view()),
     path('team/',views.TeamListView.as_view(),name='teamlistview'),
@@ -15,23 +17,16 @@ urlpatterns = [
     path('contact/',views.ContactFormView.as_view(),name='contactformview'),
     path('profilepic/',views.ProfilePic.as_view(),name='profilepic'),
     
-  
-  
     path('coursecategory/',views.CourseCategoryView.as_view(),name='coursecategory'),
     path('teachermanagement/',views.TeacherListView.as_view(),name='teachermanagement'),
     path('studentmanagement/',views.StudentListView.as_view(),name='studentmanagement'),
     path('admincourse/',views.AdminCourse.as_view(),name='admincourse'),
     path('dashboard/',views.DashBoard.as_view(),name='dashboard'),
-     path('salesreport/',views.SalesReport.as_view(),name='salesreport'),
-
-
-
-  
-     path('roomid/',views.RoomId.as_view(),name='roomid'),
-     path('messages/',views.MessageView.as_view(),name='message'),
-
-
-
+    path('salesreport/',views.SalesReport.as_view(),name='salesreport'),
+    
+    path('roomid/',views.RoomId.as_view(),name='roomid'),
+    path('messages/',views.MessageView.as_view(),name='message'),
+    
     path('teachercourses/',views.TeacherCourse.as_view(),name='teachercourses'),
     path('coursedetails/',views.CourseDetailView.as_view(),name='coursedetails'),
     path('module/',views.ModuleView.as_view(),name='module'),
@@ -40,14 +35,14 @@ urlpatterns = [
     path('teacherquiz/', views.TeacherQuiz.as_view(), name='teacherquiz'),
     path('teacherquestions/', views.TeacherQuestions.as_view(), name='teacherquestions'),
     path('teacherallassignment/', views.TeacherAllAssignment.as_view(), name='teacherallassignment'),
-     path('teacherallquiz/', views.TeacherAllQuiz.as_view(), name='teacherallquiz'),
+    path('teacherallquiz/', views.TeacherAllQuiz.as_view(), name='teacherallquiz'),
     path('shedule/',views.SheduleApiView.as_view(),name='sheduleview'),
     path('teacherprofile/',views.TeacherProfileView.as_view(),name='teacherprofile'),
 
     path('create-razorpay-order/',views.OrderApiView.as_view(),name='orderview'),
     path('handle-payment-success/',views.HandlePaymentSuccessView.as_view(),name='handlepayment'),
-
     path('check-payment/',views.CheckPayment.as_view(),name='check_payment'),
+    
     path('studentcourse/',views.StudentCourseApiView.as_view(),name='studentcourse'),
     path('studentcoursedetails/',views.StudentCourseDetails.as_view(),name='studentcoursedetails'),
     path('studentmodule/',views.StudentModule.as_view(),name='studentmodule'),
@@ -56,18 +51,6 @@ urlpatterns = [
     path('studentprofile/',views.StudentProfileView.as_view(),name='studentprofile'),
     path('studentchapter/',views.StudentChapterView.as_view(),name='studentchapter'),
     path('coursecompletion/',views.CompletionPercentageAPIView.as_view(),name='coursecompletion'),
-     path('coursecertificate/',views.StudentCertificateView.as_view(),name='studentcertificate'),
+    path('coursecertificate/',views.StudentCertificateView.as_view(),name='studentcertificate'),
 
-
-
-
- 
-
- 
-
-
-
-
-
-   
 ]
